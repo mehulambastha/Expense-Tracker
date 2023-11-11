cdec1502cc50c69e4a8f873861412533f3ea5823
mehulambastha <mehul.213amb@gmail.com>
2023-11-11 22:56:03 +0530

    working on the dashboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <h2 className="card-title">{register ? <span>Register</span> : <span>Login</span>} to continue</h2>
         <div className="form-control w-full max-w-xs">
           {/* Input form */}
-          {!register ? <p>Haven&apos;t registered yet? <span className='cursor-pointer hover:underline-offset-1 link-primary' onClick={()=>setregister(true)}>Join now!</span></p> : <span></span>}
+          {!register ? <p>Haven&apos;t registered yet? <span className='cursor-pointer hover:underline-1 link-primary' onClick={()=>setregister(true)}>Join now!</span></p> : <span></span>}
 
           {register ? <RegisterUser registerHandler = {handleRegister}/> : <LoginUser />}          
         </div>
